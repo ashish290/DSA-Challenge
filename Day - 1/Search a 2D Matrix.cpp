e541864d2deae61ede895854199134c0bb9e9b05
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-07-14 23:00:07 +0530

    Update Search a 2D Matrix.cpp

--- Day - 1/Search a 2D Matrix.cpp
+++ Day - 1/Search a 2D Matrix.cpp
@@ -1,21 +1,29 @@
-class Solution
-{
+class Solution {
 public:
-    bool searchMatrix(vector<vector<int>> &matrix, int target)
-    {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        if(matrix.empty() || matrix[0].empty()){
+            return false;
+        }
         int row_size = matrix.size();
         int col_size = matrix[0].size();
+      
+        int start = 0;
+        int end = (row_size * col_size) - 1;
 
-        for (int row = 0; row < row_size; row++)
-        {
-            for (int col = 0; col < col_size; col++)
-            {
-                if (target == matrix[row][col])
-                {
-                    return true;
-                }
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(matrix[mid/col_size] [mid%col_size] == target){
+                return true;
             }
-        }
+            else if(target > matrix[mid/col_size] [mid%col_size]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        } 
         return false;
     }
 };
