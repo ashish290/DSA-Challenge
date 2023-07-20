ac1cb717fee0838be073e0e3b2ee6edfda2d2c82
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-07-20 11:50:52 +0530

    Update Intersection of Two Linked Lists.cpp

--- Day - 6/Intersection of Two Linked Lists.cpp
+++ Day - 6/Intersection of Two Linked Lists.cpp
@@ -8,18 +8,41 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp;
-        while(headA != NULL){
-            temp = headB;
-            while(temp != NULL) {
-                if(headA == temp){
-                    return headA;
-                }
-                temp = temp->next;
-            }
-            headA= headA->next;
+    int FindLength(ListNode *temp) {
+        int cnt = 1;
+        while(temp != NULL) {
+            cnt++;
+            temp = temp->next;
         }
+        return cnt;
+    }
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int l1 = FindLength(headA);
+        int l2 = FindLength(headB);
+        int size = abs(l1-l2);
+        
+        if(l1 > l2) {
+            for(int i = 1; i <= size; i++){
+                headA = headA->next;
+            }
+        }
+        else {
+            for(int i = 1; i <= size; i++) {
+                headB = headB->next;
+            }
+        }
+
+        while(headA != NULL) {
+            if(headA == headB) {
+                return headA;
+            }
+            else {
+                headA = headA->next;
+                headB = headB->next;
+            }
+        }
+
         return NULL;
     }
 };
