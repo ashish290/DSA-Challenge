9676ef689b7a7d0ce5b38f2d659588348c4f6005
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-07-21 21:43:24 +0530

    Add files via upload

---
+++ Day - 8/Squares of a Sorted Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int>ans;
+        int temp = 0;
+        for(int i = 0; i < nums.size(); i++){
+            ans.push_back(nums[temp] * nums[temp]);
+            temp++;
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
