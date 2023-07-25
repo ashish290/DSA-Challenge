4e04ffeba5cb4bc40fc23883e39697f2688bd8eb
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-07-25 22:09:26 +0530

    Update Remove All Occurrences of a Substring.cpp

--- Day - 12/Remove All Occurrences of a Substring.cpp
+++ Day - 12/Remove All Occurrences of a Substring.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
         int n = s.length();
+//Erase is the function which used in cpp for string to erase the element from string.
+        
         while(n != 0 && s.find(part) < n){
             s.erase(s.find(part),part.length());
         }
