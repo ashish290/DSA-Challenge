4f2491b6a11dd31351cd0e5c855ef88c6b7b63bf
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-09-04 22:38:54 +0530

    Add files via upload

---
+++ Day - 53/Linked List Cycle.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head){
+            return false;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(slow->next != NULL && fast->next != NULL && fast->next->next != NULL) {
+            if(fast->next == NULL) return false;
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) 
+            return true;
+        }
+        return false;
+    }
+};
