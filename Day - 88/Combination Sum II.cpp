75f6b4db033d5d0a576271487007f83eee2cadf2
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-10-10 10:15:00 +0530

    Add files via upload

---
+++ Day - 88/Combination Sum II.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& temp, int idx) {
+            if(target == 0){
+            ans.push_back(temp);
+            return;
+            }
+        for(int i = idx; i < candidates.size(); i++) {
+            if(i > idx && candidates[i] == candidates[i-1])
+            continue;
+            if(target < candidates[i])
+            break;
+            temp.push_back(candidates[i]);
+            solve(candidates,target - candidates[i],ans,temp,i + 1);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>ans;
+        vector<int>temp;
+        solve(candidates,target,ans,temp,0);
+        return ans;
+    }
+};
