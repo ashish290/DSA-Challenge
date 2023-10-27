ec26a0f7625ee40477ab7e87e91e21d15b07ecb5
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-10-28 03:33:58 +0530

    Add files via upload

---
+++ Day - 105/Longest Palindromic Substring.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkPalindrome(string &s,int i,int j) {
+        while(i < j) {
+            if(s[i] != s[j])
+            return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int start = 0;
+        int max_len = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(checkPalindrome(s,i,j)) {
+                    if(j-i+1 > max_len) {
+                        max_len = j-i+1;
+                        start = i;
+                    }
+                 }
+               }
+            }
+        return s.substr(start,max_len);
+    }
+};
