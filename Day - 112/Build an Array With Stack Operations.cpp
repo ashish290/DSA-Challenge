ec943308ec18a70aa17a4a52d0e0ded6dde3348c
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-11-04 03:05:02 +0530

    Add files via upload

---
+++ Day - 112/Build an Array With Stack Operations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string>ans;
+        int num = 1;
+        int i = 0;
+        while(i != target.size()) {
+            if(target[i] == num) {
+                ans.push_back("Push");
+                i++;
+            }
+            else {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+            num++;
+        }
+        return ans;
+    }
+};
