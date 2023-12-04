e4c40a2cc1747789af6c52ba2d10d41d421cc380
Ashish Kumar <93868163+ashish290@users.noreply.github.com>
2023-12-04 07:29:38 +0530

    Add files via upload

---
+++ Day - 142/2951. Find the Peaks.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int>result;
+        int n = mountain.size();
+        for(int i = 1; i < n-1; i++) {
+            if(mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1])
+                result.push_back(i);
+        }
+        return result;
+    }
+};
